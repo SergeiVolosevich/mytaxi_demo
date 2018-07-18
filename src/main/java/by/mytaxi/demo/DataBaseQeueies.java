d21ca2f68632e7604b43ba510670112ce7ce9135
SergeiVolosevich <trueman230593@gmail.com>
2018-07-19 01:10:04 +0300

    My First Commit

---
+++ src/main/java/by/mytaxi/demo/DataBaseQeueies.java
@@ -0,0 +1,6 @@
+package by.mytaxi.demo;
+
+public class DataBaseQeueies {
+    public static final String SELEC_ALL_CAB="SELECT * FROM cab";
+    public static final String CALL_SHIFTS_PROCEDURE = "CALL getAllShifts";
+}
---
+++ src/main/java/by/mytaxi/demo/TaxiFirstConnection.java
@@ -0,0 +1,44 @@
+package by.mytaxi.demo;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class TaxiFirstConnection {
+    private static Logger log = LogManager.getLogger(TaxiFirstConnection.class);// prochitat pro loger;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/sql_demo";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "pass";
+
+    public static void main(String[] args) throws SQLException {
+
+        try (Connection connection = getConnection();
+           CallableStatement statement = connection.prepareCall(DataBaseQeueies.CALL_SHIFTS_PROCEDURE)) {
+
+                ResultSet rs = statement.executeQuery();
+                while (rs.next()) {
+                    log.info(rs.getString("id"));
+                }
+            }
+        }
+
+
+
+    public static Connection getConnection() throws SQLException {
+        Properties info = new Properties();
+        try(InputStream inputStream = new FileInputStream("src\\main\\resources\\database.properties")){
+            info.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(info.getProperty("jdbc.url"),info.getProperty("jdbc.user"),
+                info.getProperty("jdbc.password"));
+    }
+}
